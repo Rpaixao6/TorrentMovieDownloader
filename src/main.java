2f99a01af0f92031759f6cd8afd8222e0539168c
lopesdasilva <lopesdasilva@192.168.1.101>
2011-01-29 02:06:35 +0000

    *Introducao do JTMDB - API java para aceder a TMDB *Introducao do JSON

--- src/main.java
+++ src/main.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.util.List;
+
+import net.sf.jtmdb.Movie;
+
+import org.json.JSONException;
+
 
 public class main {
 
@@ -5,8 +12,20 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("teste");
-
-	}
+		
+		try {
+						
+			List<Movie> a = Movie.search("The Green Hornet");
+			System.out.println("Nome:"+a.get(0).getName());
+			System.out.println("Data:"+a.get(0).getReleasedDate());
+			System.out.println("IMDB ID:"+a.get(0).getImdbID());
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		}
 
 }
