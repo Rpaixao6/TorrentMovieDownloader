d5a250d1e317085a11c4f878244c8251e113996b
lopesdasilva <lopesdasilva@192.168.1.101>
2011-01-29 03:01:53 +0000

    *Metodo de limpeza do nome do torrent (ainda por fazer)

--- src/main.java
+++ src/main.java
@@ -12,14 +12,22 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		
+		cleanName("The.Green.Hornet.2010.TS.XViD-T0XiC-iNK");
+		
+		
 		try {
 						
 			List<Movie> a = Movie.search("The Green Hornet");
 			   for(int i=0; i<a.size(); i++){
-			System.out.println("Nome:"+a.get(i).getName());
-			System.out.println("Data:"+a.get(i).getReleasedDate());
-			System.out.println("IMDB ID:"+a.get(i).getImdbID());
-			   }
+		    System.out.println("------------------------------------------------");
+			System.out.println("Nome: "+a.get(i).getName());
+			System.out.println("Data: "+a.get(i).getReleasedDate());
+			System.out.println("IMDB ID: "+a.get(i).getImdbID());
+			System.out.println("Rate: "+a.get(i).getRating());
+			System.out.println("URL: "+a.get(i).getUrl());
+			System.out.println("Trailer URL: "+a.get(i).getTrailer());
+			System.out.println("Alter Name: "+a.get(i).getAlternativeName());
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -27,5 +35,10 @@ public class main {
 		
 		
 		}
+
+	private static void cleanName(String string) {
+	 //Limpar os pontos separar nome do ano e da qualidade do torrent
+		
+	}
 
 }
